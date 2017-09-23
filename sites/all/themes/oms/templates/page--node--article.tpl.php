ba6aed35d0984ed9fb237d7ef06dc2755f31bd2a
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-09-23 20:34:34 +0700

    d

--- sites/all/themes/oms/templates/page--node--article.tpl.php
+++ sites/all/themes/oms/templates/page--node--article.tpl.php
@@ -72,7 +72,7 @@
     <div id="main">
         <div class="page-title">
             <div class="container">
-                <h1 class="title"><?php echo $title?></h1>
+                <h1 class="title"><?php echo t('Tin tức')?></h1>
             </div>
         </div>
         <div class="container">
